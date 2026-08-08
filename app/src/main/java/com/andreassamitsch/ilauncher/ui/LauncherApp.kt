6b85fdd79bd77affd74592c03409301776798033
asam <47712152+andreassamitsch@users.noreply.github.com>
2026-08-08 10:00:56 +0200

    fix: use correct rememberSaveable import

--- app/src/main/java/com/andreassamitsch/ilauncher/ui/LauncherApp.kt
+++ app/src/main/java/com/andreassamitsch/ilauncher/ui/LauncherApp.kt
@@ -10,7 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.produceState
-import androidx.compose.runtime.rememberSaveable
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
