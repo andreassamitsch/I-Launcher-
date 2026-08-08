17812d985158b872f51b4958ce0bd7d10b94792c
asam <47712152+andreassamitsch@users.noreply.github.com>
2026-08-08 11:27:24 +0200

    feat: initialize update manager

--- app/src/main/java/com/andreassamitsch/ilauncher/MainActivity.kt
+++ app/src/main/java/com/andreassamitsch/ilauncher/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.andreassamitsch.ilauncher.data.apps.InstalledAppsRepository
+import com.andreassamitsch.ilauncher.data.update.UpdateManager
 import com.andreassamitsch.ilauncher.ui.LauncherApp
 import com.andreassamitsch.ilauncher.ui.theme.ILauncherTheme
 
@@ -12,10 +13,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         val installedAppsRepository = InstalledAppsRepository(applicationContext)
+        val updateManager = UpdateManager(applicationContext)
 
         setContent {
             ILauncherTheme {
-                LauncherApp(installedAppsRepository = installedAppsRepository)
+                LauncherApp(
+                    installedAppsRepository = installedAppsRepository,
+                    updateManager = updateManager,
+                )
             }
         }
     }
