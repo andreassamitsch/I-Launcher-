11cc7276e1b65a1ca298fe75c206a8fd3cde9d77
asam <47712152+andreassamitsch@users.noreply.github.com>
2026-08-08 19:14:11 +0200

    fix: import Compose ContentScale

--- app/src/main/java/com/andreassamitsch/ilauncher/ui/components/WatchNextCard.kt
+++ app/src/main/java/com/andreassamitsch/ilauncher/ui/components/WatchNextCard.kt
@@ -12,7 +12,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ContentScale
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.tv.material3.Card
