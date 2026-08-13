796bc29ddb26b9ad43095b33f5b4307aa17bd7d1
asam <47712152+andreassamitsch@users.noreply.github.com>
2026-08-13 17:08:46 +0200

    feat: provide lazy TMDB discovery loader

--- app/src/main/java/com/andreassamitsch/ilauncher/MainActivity.kt
+++ app/src/main/java/com/andreassamitsch/ilauncher/MainActivity.kt
@@ -3,6 +3,7 @@ package com.andreassamitsch.ilauncher
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.CompositionLocalProvider
 import com.andreassamitsch.ilauncher.data.apps.InstalledAppsRepository
 import com.andreassamitsch.ilauncher.data.epg.EpgRepository
 import com.andreassamitsch.ilauncher.data.openwebif.OpenWebifRepository
@@ -14,6 +15,8 @@ import com.andreassamitsch.ilauncher.data.tv.WatchNextEnrichmentRepository
 import com.andreassamitsch.ilauncher.data.tv.WatchNextRepository
 import com.andreassamitsch.ilauncher.data.update.UpdateManager
 import com.andreassamitsch.ilauncher.ui.LauncherApp
+import com.andreassamitsch.ilauncher.ui.discover.LocalTmdbDiscoveryLoader
+import com.andreassamitsch.ilauncher.ui.discover.TmdbDiscoveryLoader
 import com.andreassamitsch.ilauncher.ui.theme.ILauncherTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,6 +29,7 @@ class MainActivity : ComponentActivity() {
         val tmdbRepository = TmdbRepository(applicationContext)
         val tmdbSearchRepository = TmdbSearchRepository(applicationContext)
         val searchRepository = SearchRepository(tmdbSearchRepository)
+        val tmdbDiscoveryLoader = TmdbDiscoveryLoader(tmdbSearchRepository)
         val watchNextEnrichmentRepository = WatchNextEnrichmentRepository(tmdbRepository)
         val openWebifRepository = OpenWebifRepository(applicationContext)
         val epgRepository = EpgRepository(applicationContext, tmdbRepository)
@@ -33,16 +37,18 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             ILauncherTheme {
-                LauncherApp(
-                    installedAppsRepository = installedAppsRepository,
-                    watchNextRepository = watchNextRepository,
-                    previewChannelsRepository = previewChannelsRepository,
-                    watchNextEnrichmentRepository = watchNextEnrichmentRepository,
-                    searchRepository = searchRepository,
-                    openWebifRepository = openWebifRepository,
-                    epgRepository = epgRepository,
-                    updateManager = updateManager,
-                )
+                CompositionLocalProvider(LocalTmdbDiscoveryLoader provides tmdbDiscoveryLoader) {
+                    LauncherApp(
+                        installedAppsRepository = installedAppsRepository,
+                        watchNextRepository = watchNextRepository,
+                        previewChannelsRepository = previewChannelsRepository,
+                        watchNextEnrichmentRepository = watchNextEnrichmentRepository,
+                        searchRepository = searchRepository,
+                        openWebifRepository = openWebifRepository,
+                        epgRepository = epgRepository,
+                        updateManager = updateManager,
+                    )
+                }
             }
         }
     }
